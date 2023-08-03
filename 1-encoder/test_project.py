446b5210e06332ccdc6e16d1cbc11fd64d48ab44
yonekuramiki <yonekuramiki@yonekuramikinoMacBook-Pro.local>
2023-08-03 21:01:58 +0900

    Added project test

---
+++ 1-encoder/test_project.py
@@ -0,0 +1,27 @@
+import train
+import predict_project as predict
+from transformers import RobertaTokenizerFast, RobertaForSequenceClassification, Trainer, TrainingArguments
+
+
+# trained_model, tokenizer = train.train()
+tokenizer = RobertaTokenizerFast.from_pretrained("microsoft/codebert-base")
+#自作のtrained_model
+trained_model = RobertaForSequenceClassification.from_pretrained("trained_model-ArgoUML-mini")
+
+# コメントを用意します
+
+# ファイルからデータを読み込む
+with open('dataset/data--Ant-mini.txt', 'r') as f:
+    comments = [line.strip() for line in f.readlines()]  # 各行を読み込み、改行文字を取り除く
+
+with open('dataset/label--Ant-mini.txt', 'r') as f:
+    # "positive" を 1 に、"false" を 0 にマッピング
+    labels = [1 if line.strip() == 'positive' else 0 for line in f.readlines()]  
+
+data = [{'comment': c, 'label': l} for c, l in zip(comments, labels)]
+for i in data:
+    print(i)
+
+# SATDかどうか判定します
+predict.predict_satd(data, trained_model, tokenizer)
+
